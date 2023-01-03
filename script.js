22a0ed211f20210338cb16560a4542133a007a80
markymelnik <markemelnik@gmail.com>
2023-01-03 11:34:00 -0500

    Reconfigured code to utilize objects

--- script.js
+++ script.js
@@ -1,72 +1,101 @@
 // Library
 
-const bodyContainer = document.querySelector(".body-container");
-const addBookBtn = document.querySelector(".add-book-btn");
-const closeFormBtn = document.querySelector(".close-form-btn");
-const submitFormBtn = document.querySelector(".submit-btn");
-const bookForm = document.querySelector(".book-form");
-const bookFormTitle = document.querySelector("#title");
-const bookFormAuthor = document.querySelector("#author");
-const bookFormPages = document.querySelector("#pages");
-const bookCheckBox = document.querySelector("#readCheckbox")
-
 const myLibrary = [];
-
 let formBtnClicked = false;
 
 class Book {
-    constructor (title, author, pages, readStatus) {
-        this.title = title;
-        this.author = author;
-        this.pages = pages;
-        this.readStatus = readStatus;
-    }
+	constructor (title, author, pages, readStatus) {
+		this.title = title;
+		this.author = author;
+		this.pages = pages;
+		this.readStatus = readStatus;
+	}
 };
 
-addBookBtn.addEventListener('click', () => {
-    if (formBtnClicked) {
-        bookForm.style.visibility = "hidden";
+const formController = (() => {
+
+	const bookForm = document.querySelector(".book-form");
+
+	const initiateBookForm = () => {
+
+		const addBookBtn = document.querySelector(".add-book-btn");
+
+    addBookBtn.addEventListener('click', () => {
+    	if (formBtnClicked) {
+      	bookForm.style.visibility = "hidden";
         formBtnClicked = false;
-    } else {
+      } else {
         bookForm.style.visibility = "visible";
-        formBtnClicked = true;
-    }
-});
+      	formBtnClicked = true;
+      }
+  	})
 
-closeFormBtn.addEventListener('click', () => {
-    bookForm.style.visibility = "hidden";
-    formBtnClicked = false;
-});
+  }
 
-submitFormBtn.addEventListener('click', addBookToLibrary);
+	const closeBookForm = () => {
 
-function addBookToLibrary(event) {
+		const closeFormBtn = document.querySelector(".close-form-btn");
 
-    event.preventDefault();
+		closeFormBtn.addEventListener('click', () => {
+			bookForm.style.visibility = "hidden";
+			formBtnClicked = false;
+		})
+
+	}
+
+	const submitBookForm = () => {
+
+		const submitFormBtn = document.querySelector(".submit-btn");
+		submitFormBtn.addEventListener('click', libraryDisplayManager.addBookToLibrary);
+	}
+
+	return {
+		initiateBookForm,
+		closeBookForm,
+		submitBookForm
+	}
+
+})();
+
+const libraryDisplayManager = (() => {
+
+	const addBookToLibrary = (form) => {
+
+		const bookForm = document.querySelector(".book-form");
+		const bookFormTitle = document.querySelector("#title");
+		const bookFormAuthor = document.querySelector("#author");
+		const bookFormPages = document.querySelector("#pages");
+		const bookCheckBox = document.querySelector("#readCheckbox");
+
+		form.preventDefault();
     const newBook = new Book(bookFormTitle.value, bookFormAuthor.value, bookFormPages.value, bookCheckBox.value);
     if (bookCheckBox.checked) {
         newBook.readStatus = true;
     } else newBook.readStatus = false;
     myLibrary.push(newBook);
-    displayBooks(newBook);
+    displayBook(newBook);
     bookForm.style.visibility = "hidden";
     bookForm.reset();
     formBtnClicked = false;
-};
 
-function displayBooks(newBook) {
-    var bookCardContainer = document.createElement('div');
+	}
+
+	const displayBook = (newBook) => {
+
+		const bodyContainer = document.querySelector(".body-container");
+
+		const bookCardContainer = document.createElement('div');
     bookCardContainer.className = "bookCardContainer";
 
-    var bookCard = document.createElement('div');
+    const bookCard = document.createElement('div');
     bookCard.className = "bookCard";
     bookCard.innerHTML = `Title: ${newBook.title}<br/> Author: ${newBook.author}<br/> ${newBook.pages} Pages<br/><br/> Read  `;
 
-    var deleteBtn = document.createElement('button');
+    const deleteBtn = document.createElement('button');
     deleteBtn.className = "deleteBtn";
     deleteBtn.innerHTML = "X";
 
-    var cardCheckbox = document.createElement('input');
+    const cardCheckbox = document.createElement('input');
     cardCheckbox.type = "checkbox";
     if (newBook.readStatus) { 
         cardCheckbox.checked = true
@@ -80,10 +109,25 @@ function displayBooks(newBook) {
     deleteBtn.addEventListener('click', () => {
         bookCardContainer.remove();
     })
-    
+
     cardCheckbox.addEventListener('change', () => {
         if (cardCheckbox.checked) { 
             newBook.readStatus = true;
          } else newBook.readStatus = false;
     })
-};
+	}
+
+	return {
+		addBookToLibrary,
+		displayBook
+	}
+
+})();
+
+const appController = (() => {
+
+	formController.initiateBookForm();
+	formController.closeBookForm();
+	formController.submitBookForm();
+
+})();
